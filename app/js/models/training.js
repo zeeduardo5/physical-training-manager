473c2606966f00f590542da8e2085c14d50b31fe
zeeduardo5 <jeras24@hotmail.com>
2016-08-28 14:28:13 +0100

    validation added

--- app/js/models/training.js
+++ app/js/models/training.js
@@ -3,5 +3,16 @@ PhysicalManager.Models.Training = Backbone.Model.extend({
         time: null,
         type: null,
         date: null
+    },
+    validation: {
+        time: {
+            required: true,
+            msg: 'Please enter a valid time',
+            range: [1,24]
+        },
+        date: {
+            required: true,
+            msg: 'Please enter a valid Date'
+        }
     }
 });
--- app/js/views/trainingForm.js
+++ app/js/views/trainingForm.js
@@ -5,7 +5,26 @@ PhysicalManager.Views.TrainingForm = Backbone.View.extend({
     'submit .training-form': 'onFormSubmit'
   },
 
-  render: function() {
+  initialize: function () {
+    Backbone.Validation.bind(this, {
+      valid: function (view, attr, selector) {
+        var $el = view.$('[name=' + attr + ']'),
+          $group = $el.closest('.form-group');
+
+        $group.removeClass('has-error');
+        $group.find('.help-block').html('').addClass('hidden');
+      },
+      invalid: function (view, attr, error, selector) {
+        var $el = view.$('[name=' + attr + ']'),
+          $group = $el.closest('.form-group');
+
+        $group.addClass('has-error');
+        $group.find('.help-block').html(error).removeClass('hidden');
+      }
+    });
+  },
+  
+  render: function () {
     var html = this.template(_.extend(this.model.toJSON(), {
       isNew: this.model.isNew()
     }));
@@ -13,13 +32,17 @@ PhysicalManager.Views.TrainingForm = Backbone.View.extend({
     return this;
   },
 
-  onFormSubmit: function(e) {
+  onFormSubmit: function (e) {
     e.preventDefault();
-
-    this.trigger('form:submitted', {
+    var data = this.$('#workout-form').serializeObject();
+    this.model.set(data);
+     if(this.model.isValid(true)){
+            this.trigger('form:submitted', {
       time: this.$('.training-time-input').val(),
       type: this.$('.training-type-input').val(),
       date: this.$('.training-date-input').val()
     });
+        }
+   
   }
 });
